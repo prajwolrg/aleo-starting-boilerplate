649a9eef8ce74d0c7ebf8707d68ae2dfd0b64412
Prajwol Gyawali <mail.prajwolgyawali@gmail.com>
2023-07-31 22:46:02 +0545

    Fix test

--- src/api/account/account.test.ts
+++ src/api/account/account.test.ts
@@ -8,6 +8,8 @@ describe("POST /account/create", () => {
   const route = "/account/create";
 
   it("should return 200", async () => {
+    // Note: This test works only if the server has started
+    // Use `yarn start` to start the server
     const res = await request(BASE_URL).post(route);
     expect(res.statusCode).toBe(200);
     expect(res.body.error).toBeUndefined();
